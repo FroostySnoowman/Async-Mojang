051dbf95a24245607b087b770951c05fb6b57011
FroostySnoowman <jacob.j.beal@icloud.com>
2024-04-19 22:39:47 -0600

    Updated Setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="async-mojang",
-    version="1.0.1",
+    version="1.0.2",
     author="FroostySnoowman",
     description="An async Python wrapper for the Mojang API.",
     long_description=open('README.md').read(),
